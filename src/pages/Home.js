e7b2fa561df1756d4edf566426a99642f06cc4df
gustav <gustav.albrektson@gmail.com>
2024-03-09 15:32:09 +0100

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="root">
+            <h1>Pondering Penguin</h1>
+            <p>Hello there!</p>
+        </div>
+    );
+}
+
+export default Home;
