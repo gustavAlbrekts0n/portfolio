73036e8d7be57bc48c0e42b9b29a9be90fc8690a
gustav <gustav.albrektson@gmail.com>
2024-03-19 12:32:50 +0100

    Add more information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,12 +16,12 @@ function Home() {
                 </div>
             </div>
             <div className="projects">
-                <h1 className="projects-header">MY PROJECTS</h1>
+                <h1 className="projects-header">MY GAMES</h1>
                 <div className="project-list">
                     <Project
                         title="Snowscaper"
                         url="https://ponderingpenguin.itch.io/snowscaper"
-                        desc="Manage your very own little ski resort. Make sure the visitors are having their best time at your resort and that everything is running smoothly."
+                        desc="Manage your very own little ski resort. Make sure the visitors are having their best time at your resort and that everything is running smoothly. Build accommodation and restaurants, repair and upgrade lifts, why not give the slopes a try yourself?"
                     />
                     <Project
                         title="Freestile"
@@ -31,32 +31,32 @@ function Home() {
                     <Project
                         title="Snow Vacation"
                         url="https://ponderingpenguin.itch.io/ski-resort-manager-wip"
-                        desc="Become the manager of your very own ski resort"
+                        desc="Become the manager of your very own ski resort!"
                     />
                     <Project
                         title="Habits"
                         url="https://ponderingpenguin.itch.io/habits"
-                        desc="The world's most simplified life simulator"
+                        desc="The world's most simplified life simulator. Entertain yourself, go to work, eat various meals, sleep, repeat. Made for the Ludum Dare 47 with the theme 'stuck in a loop'."
                     />
                     <Project
                         title="Exosphere"
                         url="https://gus-028.itch.io/exosphere"
-                        desc="Aim for the stars in this minimalistic endless runner"
+                        desc="Aim for the stars in this minimalistic endless runner. Make your way through a swarm of incoming obstacles without crashing. Try to reach for the highest altitude possible!"
                     />
                     <Project
                         title="Pivot Jump"
                         url="https://gus-028.itch.io/pivot-jump"
-                        desc="Twist and turn your entire surroundings to reach the goal"
+                        desc="Twist and turn your entire surroundings to reach the goal. A cute but challenging neon puzzle platformer where you must take advantage of every wall and floor in order to make progress."
                     />
                     <Project
                         title="Geometry Merge"
                         url="https://gus-028.itch.io/geometry-merge"
-                        desc="Move and merge while watching your playable area shrinking"
+                        desc="Move and merge while watching your playable area shrinking. Control one block at a time and try to match it with a similar block, without using up all of your moves. Originally made for the Ludum Dare 42."
                     />
                     <Project
                         title="Mecha Serpent"
                         url="https://gus-028.itch.io/mecha-serpent"
-                        desc="Grow a mechanical snake large enough to reach around the globe"
+                        desc="Grow a mechanical snake large enough to reach around the globe. This game was made for the JimJam #2."
                     />
                 </div>
             </div>
