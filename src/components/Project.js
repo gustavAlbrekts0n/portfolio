8c040cd436408bcfbd3f791d1c279c1516d644d6
gustav <gustav.albrektson@gmail.com>
2024-03-19 12:25:26 +0100

    Add all projects, fix design

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,10 +2,22 @@ import "pages/Home.css";
 
 function ProjectImage(props) {
     switch (props.title) {
+        case "Snowscaper":
+            return (<img src={require("img/banner-snowscaper.png")} alt={props.title} />);
         case "Freestile":
             return (<img src={require("img/banner-freestile.png")} alt={props.title} />);
         case "Snow Vacation":
             return (<img src={require("img/banner-snowvacation.png")} alt={props.title} />);
+        case "Habits":
+            return (<img src={require("img/banner-habits.png")} alt={props.title} />);
+        case "Exosphere":
+            return (<img src={require("img/banner-exosphere.png")} alt={props.title} />);
+        case "Pivot Jump":
+            return (<img src={require("img/banner-pivotjump.png")} alt={props.title} />);
+        case "Geometry Merge":
+            return (<img src={require("img/banner-geometrymerge.png")} alt={props.title} />);
+        case "Mecha Serpent":
+            return (<img src={require("img/banner-mechaserpent.png")} alt={props.title} />);
         default:
             return (<img src={require("img/ponderingPenguin256.png")} alt={props.title} />);
     }
@@ -14,8 +26,11 @@ function ProjectImage(props) {
 function Project(props) {
     return (
         <div className="project">
-            <ProjectImage title={props.title} />
-            <h1 className="project-title">{props.title}</h1>
+            <a href={props.url} target="_blank" rel="noopener noreferrer"> <ProjectImage title={props.title} /> </a>
+            <div className="project-info">
+                <h1 className="project-title">{props.title}</h1>
+                <p className="project-desc">{props.desc}</p>
+            </div>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,8 +18,46 @@ function Home() {
             <div className="projects">
                 <h1 className="projects-header">MY PROJECTS</h1>
                 <div className="project-list">
-                    <Project title="Freestile" />
-                    <Project title="Snow Vacation" />
+                    <Project
+                        title="Snowscaper"
+                        url="https://ponderingpenguin.itch.io/snowscaper"
+                        desc="Manage your very own little ski resort. Make sure the visitors are having their best time at your resort and that everything is running smoothly."
+                    />
+                    <Project
+                        title="Freestile"
+                        url="https://ponderingpenguin.itch.io/freestile"
+                        desc="Move around the board and merge blocks by careful planning. Aim for as many juicy points as possible!" 
+                    />
+                    <Project
+                        title="Snow Vacation"
+                        url="https://ponderingpenguin.itch.io/ski-resort-manager-wip"
+                        desc="Become the manager of your very own ski resort"
+                    />
+                    <Project
+                        title="Habits"
+                        url="https://ponderingpenguin.itch.io/habits"
+                        desc="The world's most simplified life simulator"
+                    />
+                    <Project
+                        title="Exosphere"
+                        url="https://gus-028.itch.io/exosphere"
+                        desc="Aim for the stars in this minimalistic endless runner"
+                    />
+                    <Project
+                        title="Pivot Jump"
+                        url="https://gus-028.itch.io/pivot-jump"
+                        desc="Twist and turn your entire surroundings to reach the goal"
+                    />
+                    <Project
+                        title="Geometry Merge"
+                        url="https://gus-028.itch.io/geometry-merge"
+                        desc="Move and merge while watching your playable area shrinking"
+                    />
+                    <Project
+                        title="Mecha Serpent"
+                        url="https://gus-028.itch.io/mecha-serpent"
+                        desc="Grow a mechanical snake large enough to reach around the globe"
+                    />
                 </div>
             </div>
         </div>
